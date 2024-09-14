82b273cfaa19859309544a41f062f8e06a2e1023
Danil Hatin <kipsa25@mail.ru>
2024-09-15 01:46:46 +0500

    fix

--- config/tg-bot.js
+++ config/tg-bot.js
@@ -1,20 +1,49 @@
 const { Telegraf } = require('telegraf');
+const axios = require('axios');
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('welcome STRAPI WORKS!'));
 bot.command('start', (ctx) => ctx.reply('ping'));
 
-bot.launch(
-    {
-        webhook: {
-            domain: 'taroai-546ac6a4db3b.herokuapp.com/api/payment',
-            path: '/telegraf/' + process.env.WEBHOOK_SECRET
+const checkServerReady = async () => {
+    try {
+        await axios.get(`${process.env.STRAPI_URL}`);
+        return true;
+    }catch(e) {
+        return false
+    }
+}
+
+const setupWebhook = async () => {
+    const maxRetries = 10;
+    let retries = 0;
+
+    while (retries < maxRetries) {
+        if (await checkServerReady()) {
+            try {
+                await bot.telegram.setWebhook(`${process.env.STRAPI_URL}/api/payment/telegraf/${process.env.WEBHOOK_SECRET}`);
+                console.log('Вебхук успешно установлен.');
+                return;
+            } catch (error) {
+                console.error('Ошибка установки вебхука:', error.message);
+            }
+        } else {
+            console.log('Сервер еще не готов, повторная попытка через 5 секунд...');
+            await new Promise(res => setTimeout(res, 5000)); // Ждем 5 секунд перед повторной проверкой
+            retries++;
         }
     }
-).then(() => console.log('Telegram bot is running...'))
-    .catch(error => console.error('Failed to start bot:', error));
 
+    console.error('Не удалось установить вебхук после нескольких попыток.');
+};
+
+
+bot.launch().then(() => {
+    console.log('Бот Telegraf запущен.');
+    
+}).catch(error => console.error('Ошибка запуска бота:', error));
+setupWebhook(); // Установка вебхука после запуска бота
 module.exports = bot;
 
 
