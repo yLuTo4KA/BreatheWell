4b6593976132285501180fcdeac7a39f5587d32b
Danil Hatin <kipsa25@mail.ru>
2024-09-16 01:43:26 +0500

    fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,21 @@
+const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-    return {
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapi'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          ssl: { rejectUnauthorized: false },
-        },
-        debug: env.bool('DATABASE_DEBUG', false),
-      },
-    };
-  };
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
   
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
+      },
+      debug: false,
+    },
+  }
+};
+
