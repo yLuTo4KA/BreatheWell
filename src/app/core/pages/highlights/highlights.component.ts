1b499e7f6f1081494afdb591ecde415467726e8d
Danil Hatin <kipsa25@mail.ru>
2024-09-23 10:32:41 +0500

    add highlights and haptic

--- src/app/core/pages/highlights/highlights.component.ts
+++ src/app/core/pages/highlights/highlights.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CourseService } from '../../services/course.service';
 import { Subscription } from 'rxjs';
 import { Highlight } from '../../models/highlight.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-highlights',
@@ -10,33 +11,57 @@ import { Highlight } from '../../models/highlight.model';
 })
 export class HighlightsComponent {
   private courseService = inject(CourseService);
+  private router = inject(Router);
 
   highlights!: Highlight[];
+  viewConfrimModal: boolean = false;
 
   slide: number = 1;
+  currentLesson = this.courseService.getCurrentLessonId();
 
-  constructor(){}
+  constructor() { }
 
   ngOnInit(): void {
     this.courseService.lesson$.subscribe(response => {
-      if(response?.highlights) {
+      if (response?.highlights && response.id === this.currentLesson) {
         this.highlights = response.highlights;
         this.courseService.learnLesson(response.id).subscribe();
       }
     })
-    
+
   }
-
-  ngAfterViewInit(): void {
-    window.scrollTo(0, 0);
-  }
-
-  
-
   nextSlide(): void {
     this.slide++;
+  }
+  prevSlide(): void {
+    if (this.slide >= 0) {
+      this.slide--;
+    }
   }
   setSlide(id: number): void {
     this.slide = id;
   }
+
+  handleClosePage(event: MouseEvent): void {
+    event.stopPropagation();
+    this.viewConfrimModal = true;
+  }
+
+  handleClick(event: MouseEvent): void {
+    const x = event.clientX;
+    const screenWidth = window.innerWidth;
+
+    if (x >= screenWidth * 0.5) {
+      console.log(this.highlights.length);
+      console.log(this.slide);
+      if (this.highlights.length > this.slide) {
+        this.nextSlide();
+      } else {
+        this.router.navigate(['/home'])
+      }
+    } else {
+      this.prevSlide();
+    }
+  }
+
 }
