1400dac2690521d0f70e2f2d9554558058de4b45
Danil Hatin <kipsa25@mail.ru>
2024-10-09 00:46:32 +0500

    fix cron

--- config/cron-lesson.js
+++ config/cron-lesson.js
@@ -1,7 +1,7 @@
 const bot = require("./tg-bot");
 
 module.exports = {
-    "46 22 * * *": async ({ strapi }) => {
+    "0 20 * * *": async ({ strapi }) => {
         try {
             const incompleteUsers = await strapi.db.query('api::course-progress.course-progress').findMany({
                 where: {
