4676ddcbfdb700b9784f0cb6e251ac13ab379ee8
Danil Hatin <kipsa25@mail.ru>
2024-10-15 01:37:58 +0500

    fix sort audio lesson

--- src/api/audio-lesson/controllers/audio-lesson.js
+++ src/api/audio-lesson/controllers/audio-lesson.js
@@ -18,8 +18,8 @@ module.exports = createCoreController('api::audio-lesson.audio-lesson', ({ strap
                 },
             });
             function getAudio(audio) {
-                if(!audio.free) {
-                    if(user.premium) {
+                if (!audio.free) {
+                    if (user.premium) {
                         return baseUrl + audio.audio.url;
                     } else {
                         return null;
@@ -28,7 +28,16 @@ module.exports = createCoreController('api::audio-lesson.audio-lesson', ({ strap
                     return baseUrl + audio.audio.url
                 }
             }
-            const formateData = audioLessons.sort((a, b) => a.id - b.id).map(audio => {
+            const sortedLessons = audioLessons.toSorted((a, b) => {
+                if (a.free !== b.free) {
+                    return b.free - a.free;
+                }
+                if (a.opens_with === 0 && b.opens_with !== 0) return -1;
+                if (a.opens_with !== 0 && b.opens_with === 0) return 1;
+                return a.opens_with - b.opens_with;
+            });
+            
+            const formateData = sortedLessons.map(audio => {
                 return {
                     id: audio.id,
                     title: audio.title,
@@ -38,7 +47,9 @@ module.exports = createCoreController('api::audio-lesson.audio-lesson', ({ strap
                     free: audio.free,
                     opens_with: audio.opens_with
                 }
-            })
+            });
+            
+            
             ctx.send(formateData ?? null);
         } catch (e) {
             ctx.throw(500, e);
